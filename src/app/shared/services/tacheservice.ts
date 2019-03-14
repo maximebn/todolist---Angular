99e69f12fc985d578f211c7fe05171d1a8a054bd
maximebn <maximebn@posteo.ru>
2019-03-14 10:42:49 +0100

    last merge

--- src/app/shared/services/tacheservice.ts
+++ src/app/shared/services/tacheservice.ts
@@ -8,37 +8,35 @@ import { Injectable , Input} from '@angular/core';
 @Injectable({
     providedIn: 'root'
   })
-
-
 export class TacheService{
-   tacheBehaviorSubject: BehaviorSubject<TacheInterface> = new BehaviorSubject<TacheInterface>({})
-    constructor(private httpClient: HttpClient) { }
+   tacheBehaviorSubject: BehaviorSubject<TacheInterface>= new BehaviorSubject<TacheInterface>({})
 
+    constructor(private httpClient: HttpClient){
 
-
-
+    }
 public getRemoteTaches(page): Observable<TacheInterface[]> {
         return this.httpClient.get<TacheInterface[]>(
-          environment.apiRoot + 'tache/' + page);
+          environment.apiRoot+'tache/'+page)
       }
 
-public updateTache(tache: TacheInterface): Observable<any>{
+public updateTache(tache:TacheInterface) :Observable<any>{
   const tacheDto = JSON.stringify(tache);
-  return this.httpClient.put(environment.apiRoot + 'tache/update', tacheDto);
+console.log(tacheDto);
+  return this.httpClient.put(environment.apiRoot+ 'tache/update', tacheDto )
 }
 
 public deleteOne(tache: TacheInterface){
+
   return this.httpClient.delete(environment.apiRoot + 'tache/deleteOne?idTache=' + tache.id);
 }
 
 public remplaceTacheSubject(tache: TacheInterface){
   this.tacheBehaviorSubject.next(tache);
 }
-
 public addTask(tache: TacheInterface): Observable<TacheInterface> {
   const uri: string = environment.apiRoot + 'tache/save';
-  const tachejson = JSON.stringify(tache);
-  return this.httpClient.post<any>(uri, tachejson);
-}
+  const tachejson=JSON.stringify(tache);
 
+  return this.httpClient.post<any>(uri, tachejson)
+}
 }
